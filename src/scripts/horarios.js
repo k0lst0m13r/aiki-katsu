98bbe052d8cf1c06edbd3953dac77d762df37f82
Julio Branda <kolstomier@protonmail.com>
2024-04-02 17:03:12 -0300

    probando imagenes desde public

---
+++ src/scripts/horarios.js
@@ -0,0 +1,3 @@
+const imgUrl = window.innerWidth <= 800 ? 
+	document.getElementById('clases').setAttribute('src', '/public/mobile.png') :
+	document.getElementById('clases').setAttribute('src', '/public/desktop.png');	
